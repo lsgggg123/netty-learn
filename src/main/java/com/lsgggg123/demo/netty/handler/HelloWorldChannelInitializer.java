59dea3635d68e1522b92a32f510e08008e7c1260
刘顺 <lsgggg123@gmail.com>
2021-01-31 21:53:53 +0800

    hello world demo

---
+++ src/main/java/com/lsgggg123/demo/netty/handler/HelloWorldChannelInitializer.java
@@ -0,0 +1,15 @@
+package com.lsgggg123.demo.netty.handler;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class HelloWorldChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast("httpServerCodec", new HttpServerCodec());
+        pipeline.addLast("helloWorldHandler", new HelloWorldHandler());
+    }
+}
